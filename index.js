84bc303da12e7b1663e383c39c1958ae82b0b6ca
Artem Dragunov <dragunovartem99@gmail.com>
2023-09-22 18:31:46 +0300

    Add cute visualizations

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const franc = require("franc");
 const langs = require("langs");
+const cowsay = require("cowsay");
 
 const userInput = process.argv[2];
 const languageCode = franc(userInput);
 const languageName = langs.where("3", languageCode)?.name;
 
+let output;
+
 if (!userInput) {
-	console.log("Write something!");
+	output = cowsay.say({
+		text: "Oh... you should write something, so I can guess!",
+		f: "yasuna_09",
+	});
 } else if (!languageName) {
-	console.log("I don't know!");
+	output = cowsay.say({
+		text: "Sorry, I have no ideas, can you give longer sentence?",
+		f: "yasuna_12",
+	});
 } else {
-	console.log(languageName);
+	output = cowsay.say({
+		text: `I think it is ${languageName}`,
+		f: "yasuna_04",
+	});
 }
+
+console.log(output);
