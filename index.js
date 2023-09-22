e17b8870becd8556c03d024a36c6b6081fa4251b
Artem Dragunov <dragunovartem99@gmail.com>
2023-09-22 18:15:12 +0300

    Write 3 scenarios for an answer

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const franc = require("franc");
+const langs = require("langs");
 
 const userInput = process.argv[2];
-
 const languageCode = franc(userInput);
+const languageName = langs.where("3", languageCode)?.name;
 
-console.log(languageCode);
+if (!userInput) {
+	console.log("Write something!");
+} else if (!languageName) {
+	console.log("I don't know!");
+} else {
+	console.log(languageName);
+}
